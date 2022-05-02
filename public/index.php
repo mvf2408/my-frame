90e5428b7442cb66a9ce64b4c83ec22dcbf62b52
mvf2408 <mvf2408@yandex.ru>
2022-05-02 09:56:28 +0300

    Работа с классом Router. Добавлен метод matchRoute

--- public/index.php
+++ public/index.php
@@ -9,14 +9,28 @@ $query = $_SERVER['QUERY_STRING'];
 echo '<b>Строка запроса от пользователя</b></br>';
 echo $query . '<br><br>';
 
-// Добавляем маршруты в список маршрутов
+// Добавляем маршруты в таблицу маршрутов
 Router::addRoute('posts/add', ['controller' => 'Posts', 'action' => 'add']);
 Router::addRoute('posts/', ['controller' => 'Posts', 'action' => 'index']);
 Router::addRoute('', ['controller' => 'Main', 'action' => 'index']);
 
+// Проверяем запрос на совпадение с таблицей маршрутов
+Router::matchRoute($query);
 
+// Отображаем таблицу маршрутов
 echo '<b>Таблица маршрутов</b><br>';
 printPre(Router::getRoutes());
 
+// Отображаем текущий маршрут
 echo '<b>Текущий маршрут</b><br>';
 printPre(Router::getRoute());
+
+// Отображаем результат проверки запроса на совпадение на экран
+if (Router::matchRoute($query))
+{
+    echo 'Найдено совпадение с таблицей маршрутов<br>';
+}
+else
+{
+    echo 'Совпадение не найдено. Ошибка 404<br>';
+}
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -35,4 +35,22 @@ class Router {
         return self::$route;
     }
     
+    /*
+     * Проверяет, совпадает ли введенный пользователем маршрут какому
+     * либо в таблице маршрутов
+     * return boolean
+     */
+    public static function matchRoute($url)
+    {
+        foreach (self::$routes as $pattern => $route)
+        {
+            if ($url === $pattern)
+            {
+                self::$route = $route;
+                return true;
+            } 
+        }
+        return false;
+    }
+    
 }
